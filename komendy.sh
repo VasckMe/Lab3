5dc6e7b7eb6d1994087aeac819251b03bbe00a4d
Anton <dsw52839@dsw.edu.pl>
2024-06-08 23:49:56 +0200

    Dodany komendy z git tutorialu

---
+++ komendy.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+git commit 
+git commit
+
+git checkout -b bugFix
+
+git checkout -b bugFix 
+git commit 
+git checkout main 
+git commit 
+git merge bugFix
+ 
+git checkout -b bugFix 
+git commit 
+git checkout main 
+git commit 
+git checkout bugFix 
+git rebase main
+ 
+git checkout C4
+
+git checkout bugFux^
+
+git branch -f main C6 
+git branch -f bugFix C0 
+git checkout HEAD^
+ 
+git reset HEAD~1 
+git checkout pushed 
+git revert HEAD
+ 
+git cherry-pick C3 C4 C7
+ 
+git rebase -i C1
+ 
+git checkout main 
+git cherry-pick C4
+ 
+git rebase -i C1 
+git commit —amend 
+git rebase -i C1 
+git branch -f main C3''
+ 
+git checkout main 
+git cherry-pick C2 
+git commit --amend 
+git cherry-pick C3
+ 
+git tag v0 C1 
+git tag v1 C2 
+git checkout C2
+ 
+git describe side 
+git describe bugFix 
+git commit
+ 
+git rebase main bugFix 
+git rebase bugFix side 
+git rebase side another 
+git branch -f main C7’
+ 
+git branch -f bugWork HEAD~^2~
+ 
+git branch -f three C2 
+git checkout one 
+git cherry-pick C4 C3 C2 
+git checkout two git cherry-pick C5 C4’ C3’ C2’
+ 
+git clone
+ 
+git commit 
+git checkout o/main 
+git commit
+ 
+git fetch
+ 
+git pull
+ 
+git fakeTeamwork 2 
+git commit 
+git pull
+ 
+git commit 
+git commit 
+git push
+ 
+git clone 
+git fakeTeamwork 
+git commit 
+git pull —rebase 
+git push
+ 
+git reset HEAD^ 
+git checkout -b feature C2 
+git push origin feature
+ 
+git fetch 
+git rebase o/main side1 
+git rebase side1 side2 
+git rebase side2 side3 
+git rebase side3 main 
+git push
+ 
+git checkout main 
+git pull 
+git merge side1 
+git merge side2 
+git merge side3 
+git push
+ 
+git checkout -b side o/main 
+git commit 
+git pull —rebase 
+git push
+ 
+git push origin main 
+git push origin foo
+ 
+git push origin main^:foo 
+git push origin foo:main
+ 
+git fetch origin C3:foo 
+git fetch origin C6:main 
+git checkout foo 
+git merge main
+ 
+git push origin :foo 
+git fetch origin :bar
+ 
+git pull origin C3:foo 
+git pull origin C2:side
